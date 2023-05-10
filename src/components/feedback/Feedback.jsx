bbd71040360e2645a282912c37c759ce5f31cb85
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-05-10 10:29:08 -0700

    Update Feedback.jsx

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -17,43 +17,9 @@ export const Feedback = ({ options, leaveFeedback }) => {
 };
 
 Feedback.propTypes = {
-  options: PropTypes.array.isRequired,
   leaveFeedback: PropTypes.func.isRequired,
 };
 
-
-// export const Feedback = () => {
-//     const [state, setState] = useState({
-//       good: 0,
-//       neutral: 0,
-//       bad: 0,
-//     });
-
-//   const update = type => {
-//     setState(prevState => ({
-//         ...prevState,
-//         [type]: prevState[type] + 1,
-//     }));
-// }
-
-//     return (
-//       <Container>
-//         <Title>Please, leave feedback</Title>
-//         <BtnContainer>
-//           <Button onClick={() => update('good')}>Good</Button>
-//           <Button onClick={() => update('neutral')}>Neutral</Button>
-//           <Button onClick={() => update('bad')}>Bad</Button>
-//         </BtnContainer>
-
-//         <TitleStatistics>Statistics</TitleStatistics>
-//         <ul>
-//           <li>Good: {state.good}</li>
-//           <li>Neutral: {state.neutral}</li>
-//           <li>Bad: {state.bad}</li>
-//         </ul>
-//       </Container>
-//     );
-// }
 
 
 
